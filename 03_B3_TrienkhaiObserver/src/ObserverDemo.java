3da24ae57923a555733bba090a324bfe738a1fd1
tiennv0608 <tiennv0608@gmail.com>
2021-06-09 11:32:09 +0700

    b3

---
+++ 03_B3_TrienkhaiObserver/src/ObserverDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ObserverDemo {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+        new HexObserver(subject);
+        new OctObserver(subject);
+        new BinObserver(subject);
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i<5; i++){
+            System.out.print("\nEnter a number: ");
+            subject.setState(scanner.nextInt());
+        }
+    }
+}
